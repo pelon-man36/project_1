647994e5a2a17ef2e6e8772d1ae3c089ddd309fb
Johan Ramirez <johand200505@gmail.com>
2026-06-24 14:48:49 -0400

    Added for loop that gathers user inputs

--- notebook.py
+++ notebook.py
@@ -5,5 +5,6 @@ while user_num <= 0:
     print("Cannot enter anything equal or less than 0. Try again.")
     user_num = int(input("Enter number of entries: "))
 
-for i in range(0, user_num):
-    print("This is a test")
+for i in range(1, user_num + 1):
+    print(f"Write for entry #{i}:")
+    user_entry = input("")
